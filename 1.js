54834c9640c20d20469c98c77b73522b16d07077
Sasha P <apodlednevarab@gmail.com>
2023-01-30 20:36:44 +0300

    New

--- 1.js
+++ 1.js
@@ -1,3 +1,4 @@
 const temperatureC = +prompt("Введите температуру в градусах Цельсия  ");
-const temperatureF = Math.trunc(((9 / 5) * temperatureC + 32) * 100) / 100;
+//const temperatureF = Math.trunc(((9 / 5) * temperatureC + 32) * 100) / 100; (До сотых)
+const temperatureF = Math.trunc(((9 / 5) * temperatureC + 32) * 10) / 10; // (До десятых)
 alert(`Температура в градусах Цельсия ${temperatureC} по Фаренгейту равны:  ${temperatureF}`);
